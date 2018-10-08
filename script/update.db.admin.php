c4dae5da2c86e1ace8a1bae8d5ffa41901a2163f
Lucas <lukai.rat@gmail.com>
2018-10-08 17:46:53 +0800

    update

--- script/update.db.admin.php
+++ script/update.db.admin.php
@@ -3,14 +3,17 @@
 define('IN_SYS', true);
 require '../framework/bootstrap.inc.php';
 require IA_ROOT . '../web/common/bootstrap.sys.inc.php';
+
 global $_W;
 
 
+echo "Line:".__LINE__."\n\r";
 
 $sql = 'select * from ims_users where uid = 1';
 $params[':username'] = 'admin';
 $record = pdo_fetch($sql, $params);
 
+echo "Line:".__LINE__."\n\r";
 
 /*
     File: framework/model/user.mod.php  line:
@@ -26,11 +29,14 @@ $record = pdo_fetch($sql, $params);
 
 $password = 'fc123123';
 $salt     = 'd95fd308';
+echo "Line:".__LINE__."\n\r";
 
 $password = "{$password}-{$salt}-{$_W['config']['setting']['authkey']}";
 $password = sha1($password);
+echo "Line:".__LINE__."\n\r";
 
 $sql = "update ims_users set password = '".$password."' ,salt='".$salt."' where uid=1";
 $data = pdo_query($sql);
+echo "Line:".__LINE__."\n\r";
 
 echo 'ok';
